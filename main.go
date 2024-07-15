261838ce64b839df6196d0acfe4d6ff86762f9c0
Raihan <rehanolimit@gmail.com>
2024-07-15 18:42:16 +0700

    try fix

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"log"
 	"os"
 
-	
+	"github.com/joho/godotenv"
+	"github.com/raihan1405/go-restapi/models"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/raihan1405/go-restapi/db"
 	"github.com/gofiber/fiber/v2"
 )
 
 func getPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = "8080"
 	}
 	return "0.0.0.0:" + port
 
@@ -19,26 +22,19 @@ func getPort() string {
 
 func main() {
 
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	
-	// db := db.Init()
-
+	db := db.Init()
 	app := fiber.New()
-	// app.Use(logger.New())
-	// models.Setup(db)
+	app.Use(logger.New())
+	models.Setup(db)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hei")
 	})
 
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Fatal(app.Listen("0.0.0.0:" + port))
+	log.Fatal(app.Listen(getPort()))
 }
