b1fc75d6f2585d1eebfd78d954d056451436873a
Raihan <rehanolimit@gmail.com>
2024-07-15 15:52:06 +0700

    add db

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,9 @@
 package models
 
 import(
+	"fmt"
+	"log"
+	"os"
 	"gorm.io/gorm"
 	"gorm.io/driver/mysql"
 )
@@ -8,5 +11,18 @@ import(
 var DB *gorm.DB
 
 func ConnectDatabase(){
-	database, err := gorm.Open(mysql.Open())
+
+	dbUser := os.Getenv("root")
+	dbPass := os.Getenv("dygXZHHXbSWhwoTmYCieafeSJIJknQqn")
+	dbHost := os.Getenv("monorail.proxy.rlwy.net")
+	dbName := os.Getenv("railway")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = database
+	log.Println("Database connection successfully established")
 }
