eced381099bc51be44f7f22ba6b4e91051b72b1a
Raihan <rehanolimit@gmail.com>
2024-07-15 14:39:56 +0700

    update

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
