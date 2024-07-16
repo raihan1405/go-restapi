7b73954e03b68921cf12dcfaf6374b6f3f15f1c6
Raihan <rehanolimit@gmail.com>
2024-07-16 09:01:41 +0700

    fix

--- main.go
+++ main.go
@@ -31,10 +31,10 @@ func main() {
 	
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-        AllowOrigins:     "*",
-        AllowCredentials: true,
-        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowOrigins:     "https://go-restapi-production.up.railway.app:8080", 
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 	}))
 	
 
