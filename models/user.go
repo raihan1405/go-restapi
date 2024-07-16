fd4171ff66c101c0c6fb667d18f90050f91ec610
Raihan <rehanolimit@gmail.com>
2024-07-16 11:40:38 +0700

    add phoneNumber

--- models/user.go
+++ models/user.go
@@ -1,8 +1,10 @@
 package models
 
 type User struct {
+	
 	ID       int
 	Email string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
 	Username string `json:"username"`
 	Password []byte `json:"password"`
 }
