d985932efa53339d86ca2868073e1981b37cf8ec
Raihan <rehanolimit@gmail.com>
2024-07-29 14:50:15 +0700

    fix

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 	"github.com/gofiber/swagger"
 	"github.com/joho/godotenv"
 	"github.com/raihan1405/go-restapi/db"
+	_ "github.com/raihan1405/go-restapi/docs"
 	"github.com/raihan1405/go-restapi/models"
 	"github.com/raihan1405/go-restapi/routes"
-	_ "github.com/raihan1405/go-restapi/docs"
 )
 
 func getPort() string {
@@ -23,21 +23,20 @@ func getPort() string {
 	return "0.0.0.0:" + port
 
 }
-//	@title			Swagger Example API
-//	@version		1.0
-//	@description	This is a sample server celler server.
-//	@termsOfService	http://swagger.io/terms/
+
+// @title			Swagger Example API
+// @version		1.0
+// @description	This is a sample server celler server.
+// @termsOfService	http://swagger.io/terms/
 func main() {
 
-	
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	
-	
+
 	app := fiber.New()
-	allowedOrigins := "http://localhost:5173,https://sjr-app-dev-6rs5.vercel.app/"
+	allowedOrigins := "http://localhost:5173,https://sjr-app-dev-6rs5.vercel.app"
 	allowedOriginsList := strings.Split(allowedOrigins, ",")
 
 	// Set up CORS middleware with dynamic origin checking
@@ -54,7 +53,6 @@ func main() {
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 	}))
-	
 
 	db.Init()
 	models.Setup(db.DB)
