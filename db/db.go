ab86c4b5756ce021aa51ed090cb3bdc47577a782
Raihan <rehanolimit@gmail.com>
2024-08-23 08:46:10 +0700

    fix

--- db/db.go
+++ db/db.go
@@ -3,7 +3,9 @@ package db
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,22 +13,38 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	// Hardcoded values for local MySQL
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Retrieve the MySQL credentials from the .env file
+	mysqlUser := os.Getenv("MYSQLUSER")
+	mysqlPassword := os.Getenv("MYSQLPASSWORD")
+	mysqlHost := os.Getenv("MYSQLHOST")
+	mysqlPort := os.Getenv("MYSQLPORT")
+	mysqlDatabase := os.Getenv("MYSQLDATABASE")
+
+	// Construct the DSN (Data Source Name) with the environment variables
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		"root",      // MySQL Username, biasanya "root" untuk lokal
-		"password",  // MySQL Password, masukkan password MySQL Anda
-		"127.0.0.1", // Host, gunakan "127.0.0.1" atau "localhost" untuk lokal
-		"3306",      // MySQL port, default adalah "3306"
-		"kp",        // Nama database yang ingin Anda hubungkan
+		mysqlUser,     // MySQL Username
+		mysqlPassword, // MySQL Password
+		mysqlHost,     // MySQL Host
+		mysqlPort,     // MySQL Port
+		mysqlDatabase, // MySQL Database name
 	)
 
+	// Open a connection to the MySQL database
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	// Handle any errors that occur during connection
 	if err != nil {
 		log.Fatal(err)
 		panic("Failed to connect to database")
 	}
 
+	// Assign the database connection to the global DB variable
 	DB = db
 }
